04c9b44cdef4e84f03766742770db8414ec2f1f8
Karol <kurekdev@gmail.com>
2022-09-25 23:25:28 +0200

    debug false staticfiles problem 9

--- portfoliosite/settings.py
+++ portfoliosite/settings.py
@@ -134,5 +134,6 @@ MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/uploads/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
