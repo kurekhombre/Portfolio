493d69f6974f74a8ac24107973328ad4ac381d37
Karol Kurek <57529221+kurekhombre@users.noreply.github.com>
2023-01-02 23:59:22 +0100

    Update settings.py

--- portfoliosite/settings.py
+++ portfoliosite/settings.py
@@ -83,8 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD' = config('DATABASE_PASSWORD')
-        'PASSWORD': 'QTvtwYJym42rl0bS2ksh',
+        'PASSWORD' = config('DATABASE_PASSWORD'),
         'HOST': 'containers-us-west-110.railway.app',
         'PORT': '5803',
     }
